79074e9dae7a50598870d93da889946d31ab4f19
niviamg <niviamg@gmail.com>
2019-08-18 11:51:01 -0300

    Filtro de tarefas

--- tasks/src/screens/Agenda.js
+++ tasks/src/screens/Agenda.js
@@ -2,7 +2,10 @@ import React, {Component} from 'react'
 import {StyleSheet, Text, View, ImageBackground, FlatList, TouchableOpacity, Platform} from 'react-native'
 import moment from 'moment'
 import 'moment/locale/pt-br'
-import todyImage from '../../assets/imgs/today.jpg'
+import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
 import comomonStyles from '../commonStyles'
 import Tasks from '../components/Tasks'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -71,7 +74,7 @@ export default class Agenda extends Component {
 
     loadTasks = async () => {
         try{
-            const maxDate = moment().format('YYYY-MM-DD 23:59')
+            const maxDate = moment().add({days: this.props.daysAhead}).format('YYYY-MM-DD 23:59')
             const res = await axios.get(`${server}/tasks?date=${maxDate}`)
             this.setState({tasks: res.data}, this.filterTasks)
         }catch(err){
@@ -80,20 +83,43 @@ export default class Agenda extends Component {
     } 
  
     render(){
+        let styleColor =null
+        let image = null
+        switch(this.props.daysAhead){
+            case 0 : 
+                styleColor= comomonStyles.colors.today
+                image = todayImage
+                break
+            case 1:
+                styleColor = comomonStyles.colors.tomorrow
+                image = tomorrowImage
+                break
+            case 7:
+                styleColor = comomonStyles.colors.week
+                image = weekImage
+                break
+            default:
+                styleColor =comomonStyles.colors.month
+                image = monthImage
+                break
+        }
         return(
             <View style={styles.container}>
                 <AddTask isVisible={this.state.showAddTask}
                     onSave={this.addTask}
                     onCancel={()=>this.setState({showAddTask: false})} />
-                <ImageBackground source={todyImage} style={styles.background}>
+                <ImageBackground source={image} style={styles.background}>
                     <View style={styles.iconBar}>
-                        <TouchableOpacity onPress={this.toggleFilter}>
+                        <TouchableOpacity onPress ={() => this.props.navigation.openDrawer()}>
+                            <Icon name='bars' size={20} color={comomonStyles.colors.secondary} />
+                        </TouchableOpacity>
+                        <TouchableOpacity  onPress={this.toggleFilter}>
                             <Icon name={this.state.showDoneTasks?'eye':'eye-slash'}
                                 size={20} color={comomonStyles.colors.secondary} />
                         </TouchableOpacity>
                     </View>
                     <View style={styles.titleBar} >
-                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.title}>{this.props.title}</Text>
                         <Text style={styles.subtitle}>
                             {moment().locale('pt-br').format('ddd, D [de] MMMM')}
                         </Text>
@@ -106,7 +132,7 @@ export default class Agenda extends Component {
                             <Tasks {...item} onToggleTask= {this.toggleTask} 
                                 onDelete={this.deleteTask}/>}/>
                 </View>
-                <ActionButton buttonColor={comomonStyles.colors.today}
+                <ActionButton buttonColor={styleColor}
                     onPress={()=> {this.setState({showAddTask: true})}} />
             </View>
         )
@@ -145,6 +171,6 @@ const styles = StyleSheet.create({
         marginTop: Platform.os === 'ios' ? 30 : 10,
         marginHorizontal:20,
         flexDirection: 'row',
-        justifyContent:'flex-end',
+        justifyContent: 'space-between'
     }
 })
